e42139772d608e405d38e324e9954fef07f2712a
Generic35 <ricklamontagne@gmail.com>
2025-09-24 13:25:41 -0400

    feat: scaffold the app so it's ready to start loading contract data

--- src/App.js
+++ src/App.js
@@ -14,14 +14,38 @@ import TokenMaster from './abis/TokenMaster.json'
 import config from './config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+
+  const [tokenMaster, setTokenMaster] = useState(null)
+  const [occasions, setOccasions] = useState([])
+
+  const [occasion, setOccasion] = useState({})
+  const [toggle, setToggle] = useState(false)
+
+  const loadBlockchainData = async () => {
+    // TODO: Load blockchain data
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
 
   return (
     <div>
       <header>
+        <Navigation account={account} setAccount={setAccount} />
 
-        <h2 className="header__title"><strong>Welcome to Tokenmaster</strong></h2>
+        <h2 className="header__title"><strong>Event</strong> Tickets</h2>
       </header>
 
+      <Sort />
+
+      <div className='cards'>
+        {/* Occasions will go here, do not add this yet */}
+      </div>
+
+      {/* Seat chart, do not add this yet */}
     </div>
   );
 }
